bdfa77f01e4749076bcdd19480de8322968e9045
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2024-01-19 12:06:01 +0100

    find those data ids that were excluded from preprocessing in MPI study

--- src/preprocessing/annotations.py
+++ src/preprocessing/annotations.py
@@ -4,14 +4,14 @@ import numpy as np
 def annotations_checker(desc):
     """ Check Annotations """
     # basic variables
-    first_four_annots = ['New Segment/',
-                         'Comment/no USB Connection to actiCAP',
-                         'Switch',
-                         'Switch']
+    first_four_annots1 = ['New Segment/',
+                          'Comment/no USB Connection to actiCAP',
+                          'Switch',
+                          'Switch']
     annot_points = {'first_four': 0,
                     'all_same_type': 0}
     # 1. check if the first four markers' names are:
-    if (desc[:4] == first_four_annots).all():
+    if (desc[:4] == first_four_annots1).all() or desc[1] == 'Comment/actiCAP Data On':
         annot_points['first_four'] += 1
     # 2. check if all the annotations within 'Switch', are the same type
     annot_points['all_same_type'] += within_switch_checker(desc)
@@ -31,6 +31,7 @@ def change_annot_names(desc):
 
 def within_switch_checker(desc):
     # check if the annotations behave as expected
+    check = False  # default
     idxs = np.where(desc == 'Switch')[0]
     for i in range(len(idxs)-2):
         unqs = np.unique(desc[idxs[i]:idxs[i+1]])
@@ -41,6 +42,4 @@ def within_switch_checker(desc):
         else:
             if len(unqs) == 1:
                 check = True
-            else:
-                check = False
     return check

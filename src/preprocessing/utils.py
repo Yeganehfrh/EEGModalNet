969c4b21899e07c44b28322c249d72a3544d4d1a
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2024-01-22 16:45:58 +0100

    segment raw into EO/EC a new way that works fine for all files

--- src/preprocessing/utils.py
+++ src/preprocessing/utils.py
@@ -1,39 +1,6 @@
 import numpy as np
 
 
-def annotations_checker(desc):
-    """ Check Annotations """
-    # basic variables
-    first_four_annots1 = ['New Segment/',
-                          'Comment/no USB Connection to actiCAP',
-                          'Stimulus/S  1',
-                          'Stimulus/S  1']
-    annot_points = {'first_four': 0,
-                    'all_same_type': 0}
-    # 1. check if the first four markers' names are:
-    if (desc[:4] == first_four_annots1).all() or desc[1] == 'Comment/actiCAP Data On':
-        annot_points['first_four'] += 1
-    # 2. check if all the annotations within 'Switch', are the same type
-    annot_points['all_same_type'] += within_switch_checker(desc)
-    return annot_points
-
-
-def within_switch_checker(desc):
-    # check if the annotations behave as expected
-    check = False  # default
-    idxs = np.where(desc == 'Stimulus/S  1')[0]
-    for i in range(len(idxs)-2):
-        unqs = np.unique(desc[idxs[i]:idxs[i+1]])
-        if 'Stimulus/S  1' in unqs:
-            unqs = np.delete(unqs, np.where(unqs == 'Stimulus/S  1')[0])
-            if len(unqs) == 1:
-                check = True
-        else:
-            if len(unqs) == 1:
-                check = True
-    return check
-
-
 def change_annot_names(desc, enumerated=False):
     """ Change Annotations to Meaningful Names"""
     annot_name_changer = {'Stimulus/S  1': 'Switch',
@@ -54,8 +21,8 @@ def segment_raw(raw, onsets, pattern, duration=60):
     # segment raw data into EC/EO
     raws = {'EC': [], 'EO': []}
 
-    for i, v in enumerate(onsets.values()):
-        raws[pattern[i]].append(raw.copy().crop(tmin=v, tmax=v+duration))  # crop 60s after switch
+    for k, v in zip(pattern, onsets):
+        raws[k].append(raw.copy().crop(tmin=v, tmax=v+duration))  # crop 60s after switch
 
     # concatenate all EC segments
     raws['EC'][0].append(raws['EC'][1:])
@@ -68,17 +35,24 @@ def segment_raw(raw, onsets, pattern, duration=60):
     return raws
 
 
-def find_pattern(raw):
-    desc = change_annot_names(raw.annotations.description.copy(),
-                              enumerated=True)
-    onsets = raw.annotations.onset.copy()
-    annot_dict = {k: v for k, v in zip(desc[3:], onsets[3:])}  # drop first 3 annotations
+def find_switch_onset_pattern(annot_dict):
+    # only keep EC/EO markers and code them as 0/1
+    new_annot = {}
+    for k in annot_dict.keys():
+        if 'EC' in k or 'EO' in k:
+            new_annot[k] = annot_dict[k]
 
-    switch_onsets = {}
-    pattern = []
-    for i, k in enumerate(annot_dict.keys()):
-        if 'Switch' in k:
-            switch_onsets[k] = annot_dict[k]
-            # procession of EC/EO based on the next item appearing after switch
-            pattern.append(list(annot_dict.keys())[i+1].split('_')[0])
-    return switch_onsets, pattern
+    # code EC/EO as 0/1
+    keys = [0 if 'EC' in k else 1 for k in new_annot.keys()]
+
+    # where switch between EC/EO happens
+    switch = list(np.where(np.diff(np.array(keys)))[0] + 1)
+    switch = [0] + switch  # append the beginning of the first segment
+
+    # find the onsets and the pattern of the switches
+    switch_onsets = [list(new_annot.values())[i] for i in switch]
+    switch_keys = [keys[i] for i in switch]
+
+    # change back the pattern to EC/EO
+    switch_pattern = ['EC' if i == 0 else 'EO' for i in switch_keys]
+    return switch_onsets, switch_pattern

4751ddde37cc43c39e9320117a036bb3d88549b1
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2024-01-26 15:23:00 +0100

    Preprocess data within Dataloader module

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,41 +1,38 @@
+from pathlib import Path
+
+import numpy as np
 import pytorch_lightning as pl
 import torch
-import torch.nn.functional as F
+# import torch.nn.functional as F
 import xarray as xr
+from sklearn.preprocessing import RobustScaler
 from torch.utils.data import DataLoader
-from pathlib import Path
-import mne
+
+from src.preprocessing.preprocessing import correct_baseline
 
 
-
-class OtkaTimeDimSplit(pl.LightningDataModule):
+class TimeDimSplit(pl.LightningDataModule):
     """Data module to upload input data and split it into train and validation sets on
     time dimension
     """
     def __init__(self,
-                 data_dir: Path = Path('data/otka.nc5'),
+                 data_dir: Path = Path('data/eeg_eo_ec_test.nc5'),
                  train_ratio: float = 0.7,
                  segment_size: int = 128,
                  batch_size: int = 32,
-                 filter: bool = False,
-                 bandpass: list = [30, 50],
+                 data_mode: str = 'eye_open'
                  ):
         super().__init__()
         self.data_dir = data_dir
         self.train_ratio = train_ratio
         self.segment_size = segment_size
         self.batch_size = batch_size
-        self.filter = filter
-        self.bandpass = bandpass
-
+        self.data_mode = data_mode
 
     def prepare_data(self):
         # read data from file
         ds = xr.open_dataset(self.data_dir)
-        X_input = torch.from_numpy(ds['hypnotee'].values).float().permute(0, 2, 1)
-
-        if self.filter:
-            X_input = bandpass_filter(X_input, bandpass=self.bandpass)
+        X_input = torch.from_numpy(ds[self.data_mode].values).float().permute(0, 2, 1)
 
         # segment
         X_input = X_input.unfold(1, self.segment_size, self.segment_size).permute(0, 1, 3, 2)
@@ -43,21 +40,35 @@ class OtkaTimeDimSplit(pl.LightningDataModule):
         # create subject ids
         subject_ids = torch.arange(0, X_input.shape[0]).reshape(-1, 1, 1).repeat(1, X_input.shape[1], 1)
 
-        # cut point for train/test split
-        cut_point = int(X_input.shape[1] * self.train_ratio)
+        # train/test split
+        cut_point = int(X_input.shape[1] * self.train_ratio)  # cutoff
+        X_train = X_input[:, :cut_point, :, :]
+        X_test = X_input[:, cut_point:, :, :]
 
-        # train/test split & normalization
-        # TODO: should we normalize after flatening? or instead normalize over dimension 1?
-        X_train = F.normalize(X_input[:, :cut_point, :, :], dim=2)
-        X_test = F.normalize(X_input[:, cut_point:, :, :], dim=2)
+        # Pre_Process: baseline correction
+        X_train = correct_baseline(X_train).flatten(0, 1)
+        X_test = correct_baseline(X_test).flatten(0, 1)
+
+        # Pre_Process: robust scaling
+        X_train = torch.tensor(np.array(
+            [RobustScaler().fit_transform(X_train[i, :, :]) for i in range(X_train.shape[0])]
+            ))  # TODO: This scales over each segment of data independently.
+        # later we should scale over all data, expecially when switching to spliting on subject level
+        X_test = torch.tensor(np.array(
+            [RobustScaler().fit_transform(X_test[i, :, :]) for i in range(X_test.shape[0])]
+            ))
+
+        # Pre_Process: baseline correction
+        X_train = torch.clamp(X_train, min=-20, max=20)
+        X_test = torch.clamp(X_test, min=-20, max=20)
 
         self.train_dataset = torch.utils.data.TensorDataset(
-            X_train.flaten(0, 1),
+            X_train,
             subject_ids[:, :cut_point, :].flatten(0, 1),
         )
 
         self.val_dataset = torch.utils.data.TensorDataset(
-            X_test.flaten(0, 1),
+            X_test,
             subject_ids[:, cut_point:, :].flatten(0, 1),
         )
 

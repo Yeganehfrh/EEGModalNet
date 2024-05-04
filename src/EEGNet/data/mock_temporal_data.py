c6c16e71fde70910bcfa5b1a8fb5372dbdb6d2a6
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2024-05-04 12:26:53 +0200

    update target values

--- src/EEGNet/data/mock_temporal_data.py
+++ src/EEGNet/data/mock_temporal_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class MockTimeSeries():
+class MockTemporalData():
     def __init__(self, n_samples=10, n_timepoints=100, n_features=2, test_size=0.2, random_state=None):
         self.test_size = test_size
         self.n_samples = n_samples
@@ -16,22 +16,18 @@ class MockTimeSeries():
         return np.random.randint(10, 20, self.n_features)
 
     def __call__(self):
-        x = np.zeros((self.n_samples, self.n_timepoints, self.n_features))
-        freq = np.zeros((self.n_samples, self.n_features))
-        for i in range(self.n_samples):
-            freq[i] = self.generate_freq()
-            x[i] = self.generate_sample(freq[i])
+        x = np.array([self.generate_sample(self.generate_freq()) for _ in range(self.n_samples)])
         ids = np.arange(self.n_samples)
-        return x, freq, ids
+        return x, ids
 
     def train_loader(self):
-        X, freq, ids = self()
+        X, ids = self()
         return {'x': X[:self.split_point],
-                'y': freq[:self.split_point],
+                'y': ids[:self.split_point] > ids.mean(),
                 'id': ids[:self.split_point]}
 
     def test_loader(self):
-        X, freq, ids = self()
+        X, ids = self()
         return {'x': X[self.split_point:],
-                'y': freq[self.split_point:],
+                'y': ids[self.split_point:] > ids.mean(),
                 'id': ids[self.split_point:]}

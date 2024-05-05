7708e6d334a8e4095de3860470fc7cf1ecabaaac
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2024-05-05 21:05:08 +0200

    simple gan keras 3

--- src/EEGNet/models/gan.py
+++ src/EEGNet/models/gan.py
@@ -1,172 +1,101 @@
 import keras
 from keras import ops, layers
+import torch
 
 
-class Generator(keras.Model):
-    def __init__(self, latent_dim, out_channels):
-        super().__init__()
-        self.generator = keras.Sequential([
-            keras.layers.InputLayer((latent_dim,)),
-            layers.Dense(out_channels, activation='relu')
-        ],
-            name="generator")
-
-    def call(self, inputs):
-        inputs = self.generator(inputs)
-        # unflatten the inputs
-        inputs = ops.reshape(inputs, (-1, 28, 28, 1))  # TODO: change this to the correct shape
-        return inputs
-
-
-class Discriminator(keras.Model):
-    def __init__(self, in_channels):
-        super().__init__()
-        self.discriminator = keras.Sequential([
-            keras.layers.InputLayer((in_channels,)),
-            keras.layers.Flatten(),
-            layers.Dense(64, activation='relu'),
-            layers.Dense(1, activation='sigmoid')
-        ],
-            name="discriminator")
-
-    def call(self, inputs):
-        return self.discriminator(inputs)
-
-
-class GAN(keras.Model):
-    def __init__(self, latent_dim):
-        super().__init__()
+class SimpleGAN(keras.Model):
+    def __init__(self, time_dim=100, feature_dim=2, latent_dim=64, **kwargs):
+        super().__init__(**kwargs)
+        self.time = time_dim
+        self.feature = feature_dim
+        self.input_shape = (time_dim, feature_dim)
         self.latent_dim = latent_dim
-        self.generator = Generator(latent_dim, 256 * 12)
-        self.discriminator = Discriminator(256 * 12)
-        self.gen_loss_tracker = keras.metrics.Mean(name="generator_loss")
-        self.disc_loss_tracker = keras.metrics.Mean(name="discriminator_loss")
-    
-    def metrics(self):
-        return [self.gen_loss_tracker, self.disc_loss_tracker]
+        self.d_loss_tracker = keras.metrics.Mean(name='d_loss')
+        self.g_loss_tracker = keras.metrics.Mean(name='g_loss')
+        self.accuracy_tracker = keras.metrics.BinaryAccuracy(name='accuracy')
+        self.seed_generator = keras.random.SeedGenerator(42)
 
-    def compile(self, d_optimizer, g_optimizer, loss_fn):
-        super().compile()
+        self.generator = keras.Sequential([
+            keras.Input(shape=(self.latent_dim,)),
+            layers.Dense(128, activation='relu'),
+            layers.Dense(self.time * self.feature),
+            layers.Reshape(self.input_shape)
+        ], name='generator')
+
+        self.discriminator = keras.Sequential([
+            keras.Input(shape=self.input_shape),
+            layers.Flatten(),
+            layers.Dense(self.time * self.feature, activation='relu'),
+            layers.Dense(1, activation='sigmoid')
+        ], name='discriminator')
+
+        self.built = True
+
+    @property
+    def metrics(self):
+        return [self.d_loss_tracker, self.g_loss_tracker,
+                self.accuracy_tracker]
+
+    def compile(self, loss, d_optimizer, g_optimizer):
+        super().compile(loss=loss)
+        self.loss_fn = loss
         self.d_optimizer = d_optimizer
         self.g_optimizer = g_optimizer
-        self.loss_fn = loss_fn
+        # self.additional_metrics = metrics
 
-    def train_step(self, data):
-        # Unpack the data.
-        real_images, labels = data
+    def call(self, x):
+        return self.discriminator(x)
 
-        # Add dummy dimensions to the labels so that they can be concatenated with
-        # the images. This is for the discriminator.
-        labels_ = labels[:, None]
-        labels_ = ops.repeat(
-            labels_, repeats=[image_size * image_size]
-        )
-        image_one_hot_labels = ops.reshape(
-            image_one_hot_labels, (-1, image_size, image_size, num_classes)
-        )
+    def train_step(self, real_data):
+        x_real = real_data
+        batch_size = ops.shape(x_real)[0]
 
-        # Sample random points in the latent space and concatenate the labels.
-        # This is for the generator.
-        batch_size = ops.shape(real_images)[0]
-        random_latent_vectors = keras.random.normal(
-            shape=(batch_size, self.latent_dim), seed=self.seed_generator
-        )
-        random_vector_labels = ops.concatenate(
-            [random_latent_vectors, one_hot_labels], axis=1
+        noise = keras.random.normal((batch_size, self.latent_dim),
+                                    mean=0, stddev=1,
+                                    # seed=self.seed_generator
+                                    )
+
+        x_fake = self.generator(noise)
+        x = ops.concatenate([x_real, x_fake], axis=0)
+
+        # 0=real, 1=fake
+        y = ops.concatenate(
+            [ops.zeros((batch_size, 1)),
+             ops.ones((batch_size, 1))], axis=0
         )
 
-        # Decode the noise (guided by labels) to fake images.
-        generated_images = self.generator(random_vector_labels)
+        # trick that adds noise to labels
+        y = y + 0.05 * keras.random.uniform(ops.shape(y))
 
-        # Combine them with real images. Note that we are concatenating the labels
-        # with these images here.
-        fake_image_and_labels = ops.concatenate(
-            [generated_images, image_one_hot_labels], -1
-        )
-        real_image_and_labels = ops.concatenate([real_images, image_one_hot_labels], -1)
-        combined_images = ops.concatenate(
-            [fake_image_and_labels, real_image_and_labels], axis=0
-        )
+        # train discriminator
+        self.zero_grad()
+        y_pred = self.discriminator(x, training=True)
+        d_loss = self.loss_fn(y, y_pred)  # TODO use compute_loss?
+        d_loss.backward()
+        grads = [v.value.grad for v in self.discriminator.trainable_weights]
+        with torch.no_grad():
+            self.d_optimizer.apply(grads, self.discriminator.trainable_weights)
 
-        # Assemble labels discriminating real from fake images.
-        labels = ops.concatenate(
-            [ops.ones((batch_size, 1)), ops.zeros((batch_size, 1))], axis=0
-        )
+        # train generator
+        noise = keras.random.normal((batch_size, self.latent_dim),
+                                    mean=0, stddev=1,
+                                    # seed=self.seed_generator
+                                    )
 
-        # Train the discriminator.
-        with tf.GradientTape() as tape:
-            predictions = self.discriminator(combined_images)
-            d_loss = self.loss_fn(labels, predictions)
-        grads = tape.gradient(d_loss, self.discriminator.trainable_weights)
-        self.d_optimizer.apply_gradients(
-            zip(grads, self.discriminator.trainable_weights)
-        )
+        y_misleading = ops.zeros((batch_size, 1))  # saying all are real (0)
 
-        # Sample random points in the latent space.
-        random_latent_vectors = keras.random.normal(
-            shape=(batch_size, self.latent_dim), seed=self.seed_generator
-        )
-        random_vector_labels = ops.concatenate(
-            [random_latent_vectors, one_hot_labels], axis=1
-        )
+        self.zero_grad()
+        y_pred = self.discriminator(self.generator(noise))
+        g_loss = self.loss_fn(y_misleading, y_pred)
+        g_loss.backward()
+        grads = [v.value.grad for v in self.generator.trainable_weights]
+        with torch.no_grad():
+            self.g_optimizer.apply(grads, self.generator.trainable_weights)
 
-        # Assemble labels that say "all real images".
-        misleading_labels = ops.zeros((batch_size, 1))
-
-        # Train the generator (note that we should *not* update the weights
-        # of the discriminator)!
-        with tf.GradientTape() as tape:
-            fake_images = self.generator(random_vector_labels)
-            fake_image_and_labels = ops.concatenate(
-                [fake_images, image_one_hot_labels], -1
-            )
-            predictions = self.discriminator(fake_image_and_labels)
-            g_loss = self.loss_fn(misleading_labels, predictions)
-        grads = tape.gradient(g_loss, self.generator.trainable_weights)
-        self.g_optimizer.apply_gradients(zip(grads, self.generator.trainable_weights))
-
-        # Monitor loss.
-        self.gen_loss_tracker.update_state(g_loss)
-        self.disc_loss_tracker.update_state(d_loss)
+        # Update metrics and return their value.
+        self.d_loss_tracker.update_state(d_loss)
+        self.g_loss_tracker.update_state(g_loss)
         return {
-            "g_loss": self.gen_loss_tracker.result(),
-            "d_loss": self.disc_loss_tracker.result(),
+            'd_loss': self.d_loss_tracker.result(),
+            'g_loss': self.g_loss_tracker.result(),
         }
-
-
-
-    # def train_step(self, data):
-    #     x, y = data
-
-    #     # Train discriminator
-    #     with ops.GradientTape() as tape:
-    #         fake_y = self.generator(x, training=True)
-    #         fake_y = ops.stop_gradient(fake_y)
-    #         combined_y = ops.concatenate([y, fake_y], axis=0)
-    #         labels = ops.concatenate([ops.ones((y.shape[0], 1)), ops.zeros((fake_y.shape[0], 1))], axis=0)
-    #         d_loss = self.loss_fn(labels, combined_y)
-    #     d_grads = tape.gradient(d_loss, self.discriminator.trainable_weights)
-    #     self.d_optimizer.apply_gradients(zip(d_grads, self.discriminator.trainable_weights))
-
-    #     # Train generator
-    #     with ops.GradientTape() as tape:
-    #         fake_y = self.generator(x, training=True)
-    #         labels = ops.ones((x.shape[0], 1))
-    #         g_loss = self.loss_fn(labels, fake_y)
-    #     g_grads = tape.gradient(g_loss, self.generator.trainable_weights)
-    #     self.g_optimizer.apply_gradients(zip(g_grads, self.generator.trainable_weights))
-
-    #     return {"d_loss": d_loss, "g_loss": g_loss}
-
-    # def test_step(self, data):
-    #     x, y = data
-    #     fake_y = self.generator(x, training=False)
-    #     labels = ops.ones((x.shape[0], 1))
-    #     g_loss = self.loss_fn(labels, fake_y)
-    #     return {"g_loss": g_loss}
-
-    # def generate(self, inputs):
-    #     return self.generator(inputs, training=False)
-
-    # def discriminate(self, inputs):
-    #     return self.discriminator(inputs, training=False)

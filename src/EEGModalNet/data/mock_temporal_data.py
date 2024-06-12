8e8ae023551e8d147c2244ecb0b83e79cffd41a1
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2024-06-12 13:26:02 +0200

    bound the amplitude of sine waves to [0, 1]

--- src/EEGModalNet/data/mock_temporal_data.py
+++ src/EEGModalNet/data/mock_temporal_data.py
@@ -2,12 +2,14 @@ import numpy as np
 
 
 class MockTemporalData():
-    def __init__(self, n_samples=10, n_timepoints=100, n_features=2, test_size=0.2, random_state=None):
+    def __init__(self, n_samples=10, n_timepoints=100, n_features=2, test_size=0.2,
+                 clip=True):
         self.test_size = test_size
         self.n_samples = n_samples
         self.n_timepoints = n_timepoints
         self.n_features = n_features
         self.split_point = n_samples - int(n_samples * test_size)
+        self.clip = clip
 
     def generate_sample(self, freq):
         return np.sin(np.linspace(0, freq, self.n_timepoints))
@@ -19,6 +21,8 @@ class MockTemporalData():
         freqs = self.generate_freq()
         x = np.array([self.generate_sample(freqs) for _ in range(self.n_samples)])
         ids = np.arange(self.n_samples)
+        if self.clip:
+            x = (x + 1) * 0.5
         return x, ids
 
     def train_loader(self):

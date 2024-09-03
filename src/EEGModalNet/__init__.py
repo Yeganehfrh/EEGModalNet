f32e13f3eef00657931cfdfe36de3c68f07ef07c
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2024-09-03 19:21:59 +0200

    organize EEGModalNet package structure

---
+++ src/EEGModalNet/__init__.py
@@ -0,0 +1,4 @@
+from .models.WGAN import WGAN_GP
+from .utils import ProgressBarCallback
+
+__all__ = ['WGAN_GP', 'ProgressBarCallback']
--- src/EEGModalNet/models/__init__.py
+++ src/EEGModalNet/models/__init__.py
@@ -1,4 +1,5 @@
 from .multi_input import MultiInputModel
 from .gan import SimpleGAN
+from .WGAN import WGAN_GP
 
-__all__ = ['MultiInputModel', 'SimpleGAN']
+__all__ = ['MultiInputModel', 'SimpleGAN', 'WGAN_GP']

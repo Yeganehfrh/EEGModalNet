8eb674689aae2e7d2b9c5b4e18b71be5008b9f58
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2024-09-04 13:29:06 +0200

    Refactor EEGModalNet package structure and import statements

--- src/EEGModalNet/models/__init__.py
+++ src/EEGModalNet/models/__init__.py
@@ -1,7 +1,6 @@
 from .multi_input import MultiInputModel
 from .gan import SimpleGAN
 from .WGAN import WGAN_GP
-from .common import SubjectLayers_v2, convBlock, ChannelMerger, ResidualBlock
+# from .common import SubjectLayers_v2, convBlock, ChannelMerger, ResidualBlock
 
-__all__ = ['MultiInputModel', 'SimpleGAN', 'WGAN_GP',
-           'SubjectLayers_v2', 'convBlock', 'ChannelMerger', 'ResidualBlock']
+__all__ = ['MultiInputModel', 'SimpleGAN', 'WGAN_GP']

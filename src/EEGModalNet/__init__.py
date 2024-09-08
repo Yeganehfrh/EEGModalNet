c2fab0f919c0344086d0ff765c04afa71867c4ba
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2024-09-08 20:40:03 +0200

    Add a new custom callback to support saving models at regular intervals.

--- src/EEGModalNet/__init__.py
+++ src/EEGModalNet/__init__.py
@@ -3,7 +3,7 @@ os.environ['KERAS_BACKEND'] = 'torch'
 os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = '1'
 
 from .models.WGAN import WGAN_GP
-from .utils import ProgressBarCallback
+from .utils import ProgressBarCallback, CustomModelCheckpoint
 from .models.common import SubjectLayers_v2, convBlock, ChannelMerger, ResidualBlock
 
 __all__ = ['WGAN_GP',
--- src/EEGModalNet/utils.py
+++ src/EEGModalNet/utils.py
@@ -2,6 +2,21 @@ import keras
 from tqdm import tqdm
 
 
+class CustomModelCheckpoint(keras.callbacks.ModelCheckpoint):
+    def __init__(self, filepath, save_period, **kwargs):
+        super().__init__(filepath, **kwargs)
+        self.save_period = save_period
+
+    def on_epoch_end(self, epoch, logs=None):
+        super().on_epoch_end(epoch, logs)
+
+        # Every 20 epochs, save a separate model file
+        if (epoch + 1) % self.save_period == 0:
+            # Save model with a dynamic file name including the epoch number
+            filepath = self.filepath.format(epoch=epoch + 1)
+            self.model.save(filepath)
+
+
 class ProgressBarCallback(keras.callbacks.Callback):
     def __init__(self, n_epochs=None,
                  n_runs=None,

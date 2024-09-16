6e5bde354a6fcd0a68e48b8090688e90b8b7a2dc
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2024-09-16 17:56:49 +0200

    correct a typo

--- jobs/train_classifier.sh
+++ jobs/train_classifier.sh
@@ -8,7 +8,7 @@
 #SBATCH --mem=16GB
 #SBATCH --time=12:00:00
 #SBATCH --output=/work/projects/acnets/EEGModalNet/logs/train_classifier_pipeline_%j.log
-#SBATCH --error=/work/projects/acnets/EEGModalNet/logs/train_lassifier_pipeline_%j.log
+#SBATCH --error=/work/projects/acnets/EEGModalNet/logs/train_classifier_pipeline_%j.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=y.farahzadi@gmail.com
 

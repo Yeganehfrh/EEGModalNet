bb90f04d047c0c32284c4c19c3dcf9e4ff34f35a
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-01-08 12:48:47 +0100

    refactor: update data loading and model training process for multichannel support

--- src/EEGModalNet/pipeline/train_gan.py
+++ src/EEGModalNet/pipeline/train_gan.py
@@ -38,95 +38,70 @@ def load_data(data_path: str,
     n_subjects = x.shape[0]
 
     if bandpass_filter is not None:
-        sos = butter(4, bandpass_filter, btype='high', fs=128, output='sos')
+        sos = butter(4, bandpass_filter, btype='band', fs=128, output='sos')
         x = sosfiltfilt(sos, x, axis=-1)
 
-    x = torch.tensor(x).unfold(2, time_dim, time_dim).permute(0, 2, 3, 1).flatten(0, 1)
+    x = torch.tensor(x.copy()).unfold(2, time_dim, time_dim).permute(0, 2, 3, 1).flatten(0, 1)  # TODO: copy was added because of an error, look into this
 
     sub = np.arange(0, n_subjects).repeat(x.shape[0] // n_subjects)[:, np.newaxis]
 
     ch_ind = find_channel_ids(xarray, channels)
     pos = xarray.ch_positions[ch_ind][None].repeat(x.shape[0], 0)
 
-    return {'x': x[:16384], 'sub': torch.tensor(sub)[:16384], 'pos': torch.tensor(pos)[:16384]}, n_subjects  # TODO: remove hard-coded value
+    return {'x': x, 'sub': torch.tensor(sub), 'pos': torch.tensor(pos)}, n_subjects
 
 
-def run(rank,
-        world_size,
-        data,
+def run(data,
         n_subjects,
         max_epochs=100_000,
         latent_dim=64,
-        cvloger_path='tmp/tmp/simple_gan_v1.csv',
-        model_path='tmp/tmp/wgan_v2.model.keras',
+        batch_size=64,
+        cvloger_path='tmp/simple_gan_v1.csv',
+        model_path='tmp/wgan_v2',
         reuse_model=False,
         reuse_model_path=None):
 
-    # Set up the environment variables for master address and port
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
-
-    device = torch.device("cuda:{}".format(rank))
-    print(torch.cuda.is_available())
-
-    # Initialize the process group
-    dist.init_process_group(rank=rank, world_size=world_size)
-    print('initializing...')
-    torch.cuda.set_device(device)
-    print('set the device')
-
-    model = WGAN_GP(time_dim=512, feature_dim=1,
+    model = WGAN_GP(time_dim=512, feature_dim=data['x'].shape[-1],
                     latent_dim=latent_dim, n_subjects=n_subjects,
                     use_sublayer_generator=True,
                     use_sublayer_critic=False,
-                    use_channel_merger=False,
+                    use_channel_merger_c=True,
+                    use_channel_merger_g=True,
                     kerner_initializer='random_normal',
-                    interpolation='bilinear')  # TODO model.to(rank)
-
-    model.to(rank)
-
-    model = DDP(model, device_ids=[rank], output_device=rank)  # device_ids=[rank] # TODO Create the model and move it to the appropriate device
-
-    # Create the dataset and dataloader
-    sampler = DistributedSampler(data, num_replicas=world_size, rank=rank, shuffle=False)
-    dataloader = DataLoader(data, batch_size=128, sampler=sampler, shuffle=False)
-
-    print('>>>>>>> Model and DataLoader created')
+                    interpolation='bilinear')
 
     model.compile(d_optimizer=keras.optimizers.Adam(0.00005, beta_1=0.5, beta_2=0.9),
                   g_optimizer=keras.optimizers.Adam(0.0005, beta_1=0.5, beta_2=0.9),
                   gradient_penalty_weight=5.0)
 
-    for epoch in range(max_epochs):
-        for batch in dataloader:
-            x, sub, pos = batch
-            logs = model.train_step((x, sub, pos))
-            print(f"Rank {rank}, Epoch {epoch}, Batch {batch}, Logs {logs}")
+    callbacks = [CustomModelCheckpoint(model_path, save_freq=20),
+                 keras.callbacks.CSVLogger(cvloger_path, append=True),
+                 keras.callbacks.ModelCheckpoint(model_path + 'best_gloss.model.keras', monitor='g_loss', save_best_only=True)]
 
-    # Clean up
-    dist.destroy_process_group()
+    _ = model.fit(data,
+                  epochs=max_epochs,
+                  batch_size=batch_size,
+                  callbacks=callbacks)
 
-    return model, None
-
-
-def train_worker(rank, world_size, data, n_subjects, max_epochs, latent_dim, cvloger_path, model_path, reuse_model, reuse_model_path):
-    torch.set_num_threads(1)  # Limit each worker to a single thread
-    run(rank, world_size, data, n_subjects, max_epochs, latent_dim, cvloger_path, model_path, reuse_model, reuse_model_path)
+    return model
 
 
 if __name__ == '__main__':
     data, n_subs = load_data('data/LEMON_DATA/eeg_EC_BaseCorr_Norm_Clamp_with_pos.nc5',
-                             n_subjects=34, channels=['O1'], highpass_filter=1, time_dim=512,  # TODO: n_subjects=202
-                             exclude_sub_ids=['sub-010257', 'sub-010044', 'sub-010266'])
-    num_gpu = torch.cuda.device_count()
-    print(f'Running on {num_gpu} GPUs')
-    dataset = TensorDataset(data['x'], data['sub'], data['pos'])
+                             n_subjects=34, channels=['O1', 'O2', 'Fp1', 'Fp2', 'C1', 'C2', 'P1', 'P2'], bandpass_filter=[1, 42], time_dim=512,  # TODO: n_subjects=202
+                             # exclude_sub_ids=['sub-010257', 'sub-010044', 'sub-010266']
+                             )
 
-    output_path = 'logs/outputs/multiprocessing_test'
+    output_path = 'logs/multichannel_test_08-01-2025'
 
-    n_cpus = 1
-    max_epochs = 1  # 100_000
-    mp.spawn(train_worker,
-             args=(n_cpus, dataset, n_subs, max_epochs, 64, f'{output_path}.csv', output_path, False, None),
-             nprocs=n_cpus,
-             join=True)
+    max_epochs = 100
+    latent_dim = 64
+    run(data,
+        n_subs,
+        max_epochs=max_epochs,
+        latent_dim=latent_dim,
+        batch_size=64,
+        cvloger_path=f'{output_path}.csv',
+        model_path=output_path,
+        reuse_model=False,
+        reuse_model_path=None)

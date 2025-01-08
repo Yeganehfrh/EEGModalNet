tmp fix: use half precision for weights in SubjectLayers

src/EEGModalNet/models/common.py
@@ -199,10 +199,11 @@ class SubjectLayers(nn.Module):
 
     def forward(self, x, subjects):
         _, C, D = self.weights.shape
-        weights = self.weights.gather(0, subjects.view(-1, 1, 1).expand(-1, C, D))
-        if keras.mixed_precision.global_policy().name == 'mixed_float16':
-            print('Using mixed precision in SubjectLayers')
-            weights.half()
+        weights = self.weights.gather(0, subjects.view(-1, 1, 1).expand(-1, C, D)).half()  # TODO: remove .half()
+        print(x.dtype, weights.dtype)
+        # if keras.mixed_precision.global_policy().name == 'mixed_float16':
+        #     print('Using mixed precision in SubjectLayers')
+        #     weights.half()
         x_ = torch.einsum("bct,bcd->bdt", x, weights)
         return x_
 
6902851c3d825ca5b40a36cdbd845f43b91e2b20
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-01-15 11:51:54 +0100

    adjust checkpoint frequency, extend max epochs, and modify output path

--- src/EEGModalNet/pipeline/train_gan_gpu.py
+++ src/EEGModalNet/pipeline/train_gan_gpu.py
@@ -85,7 +85,7 @@ def run(data,
                         epochs=max_epochs,
                         shuffle=True,
                         callbacks=[
-                            CustomModelCheckpoint(model_path, save_freq=20),
+                            CustomModelCheckpoint(model_path, save_freq=50),
                             keras.callbacks.ModelCheckpoint(f'{model_path}_best_gloss.model.keras', monitor='g_loss', save_best_only=True),
                             keras.callbacks.ModelCheckpoint(f'{model_path}_best_dloss.model.keras', monitor='d_loss', save_best_only=True),
                             keras.callbacks.CSVLogger(cvloger_path),
@@ -122,17 +122,17 @@ if __name__ == '__main__':
     keras.mixed_precision.set_global_policy('mixed_float16')
     print(f'Global policy is {keras.mixed_precision.global_policy().name}')
 
-    output_path = 'logs/15.01.2025'
+    output_path = 'logs/round2_15.01.2025'
 
     model, _ = run(data,
                    n_subjects=n_subs,
-                   max_epochs=400,
+                   max_epochs=800,
                    latent_dim=128,
                    batch_size=128,
                    cvloger_path=f'{output_path}.csv',
                    model_path=output_path,
-                   reuse_model=False,
-                   reuse_model_path=None)
+                   reuse_model=True,
+                   reuse_model_path='logs/15.01.2025_epoch_200.model.keras')
 
     # # backup
     # model.save(f'{output_path}_final.model.keras')

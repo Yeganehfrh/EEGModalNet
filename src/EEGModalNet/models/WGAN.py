dbcd163396bd6598c95a1bcea2c33256ef60888f
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-01-17 12:52:58 +0100

    Remove gradient clipping

--- src/EEGModalNet/models/WGAN.py
+++ src/EEGModalNet/models/WGAN.py
@@ -217,9 +217,6 @@ class WGAN_GP(keras.Model):
         d_loss = (fake_pred.mean() - real_pred.mean()) + gp * self.gradient_penalty_weight
         d_loss.backward()
 
-        # # clip the gradients
-        # torch.nn.utils.clip_grad_norm_(self.critic.parameters(), max_norm=10.0)
-
         grads = [v.value.grad for v in self.critic.trainable_weights]
         with torch.no_grad():
             self.d_optimizer.apply(grads, self.critic.trainable_weights)
@@ -239,9 +236,6 @@ class WGAN_GP(keras.Model):
         fake_pred = self.critic({'x': x_gen, 'sub': sub, 'pos': pos})
         g_loss = -fake_pred.mean()
         g_loss.backward()
-
-        # # clip the gradients
-        # torch.nn.utils.clip_grad_norm_(self.generator.parameters(), max_norm=10.0)
 
         grads = [v.value.grad for v in self.generator.trainable_weights]
         with torch.no_grad():

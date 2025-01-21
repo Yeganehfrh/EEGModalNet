95482eb1807616a5efa1d8a2d7408f022d24a189
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-01-21 16:33:03 +0100

    Critic: adjusting SelfAttention parameters, adding LayerNormalization, and reducing dense layers + decrease critic updates.

--- src/EEGModalNet/models/WGAN.py
+++ src/EEGModalNet/models/WGAN.py
@@ -34,7 +34,7 @@ class Critic(keras.Model):
             layers.Conv1D(4 * feature_dim, 5, strides=2, padding='same', name='conv5', kernel_initializer=kernel_initializer),
             layers.LeakyReLU(negative_slope=negative_slope),
             LearnablePositionalEmbedding(256, 32),  # TODO: Make this more dynamic so the model can be used with different time dimensions according to the input
-            SelfAttention1D(4, 8),
+            SelfAttention1D(2, 4),
             layers.Conv1D(8 * feature_dim, 5, strides=2, padding='same', name='conv6', kernel_initializer=kernel_initializer),
             layers.LeakyReLU(negative_slope=negative_slope),
             layers.Conv1D(8 * feature_dim, 5, strides=2, padding='same', name='conv7', kernel_initializer=kernel_initializer),
@@ -45,19 +45,15 @@ class Critic(keras.Model):
             layers.LeakyReLU(negative_slope=negative_slope),
             layers.Conv1D(8 * feature_dim, 5, strides=2, padding='same', name='conv10', kernel_initializer=kernel_initializer),
             layers.LeakyReLU(negative_slope=negative_slope),
-            # LearnablePositionalEmbedding(8, 64),  # TODO: consider adding a positional embedding here before collapsing the time dimensions
+            LearnablePositionalEmbedding(8, 64),  # TODO: consider adding a positional embedding here before collapsing the time dimensions
+            SelfAttention1D(2, 4),
             layers.Flatten(name='dis_flatten'),
             layers.Dense(256, name='dis_dense0', kernel_initializer=kernel_initializer),
+            layers.LayerNormalization(),
             layers.LeakyReLU(negative_slope=negative_slope),
             layers.Dense(128, name='dis_dense1', kernel_initializer=kernel_initializer),
             layers.LeakyReLU(negative_slope=negative_slope),
             layers.Dense(64, name='dis_dense2', kernel_initializer=kernel_initializer),
-            layers.LeakyReLU(negative_slope=negative_slope),
-            layers.Dense(32, name='dis_dense3', kernel_initializer=kernel_initializer),
-            layers.LeakyReLU(negative_slope=negative_slope),
-            layers.Dense(16, name='dis_dense4', kernel_initializer=kernel_initializer),
-            layers.LeakyReLU(negative_slope=negative_slope),
-            layers.Dense(8, name='dis_dense5', kernel_initializer=kernel_initializer),
             layers.LeakyReLU(negative_slope=negative_slope),
             layers.Dense(1, name='dis_dense6', dtype='float32', kernel_initializer=kernel_initializer),
         ], name='critic')
@@ -216,7 +212,7 @@ class WGAN_GP(keras.Model):
         std = real_data.std()
 
         # train critic
-        for _ in range(3):
+        for _ in range(2):
             noise = keras.random.normal((batch_size, self.latent_dim), mean=mean, stddev=std, dtype=real_data.dtype)
             fake_data = self.generator((noise, sub, pos)).detach()  # TODO: consider using random sub
             real_pred = self.critic(data)

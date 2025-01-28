ed6df6d2aa008df47334b100d9fb70b70a4b2103
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-01-28 13:55:40 +0100

    add standard deviation metrics for real and fake predictions in WGAN

--- src/EEGModalNet/models/WGAN.py
+++ src/EEGModalNet/models/WGAN.py
@@ -257,5 +257,7 @@ class WGAN_GP(keras.Model):
             'critic_grad_norm': sum(gradient_norms) / len(gradient_norms),
             '_real_pred': real_pred.mean().item(),
             '_fake_pred': fake_pred.mean().item(),
+            '_real_pred_std': real_pred.std().item(),
+            '_fake_pred_std': fake_pred.std().item(),
             '_gp': gp.item(),
         }

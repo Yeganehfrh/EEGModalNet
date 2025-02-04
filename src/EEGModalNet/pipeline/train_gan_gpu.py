0e98cc53a290b5dd6f0534b27732d8d3a4b78638
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-02-04 19:10:31 +0100

    update output path, max epoch, and save freq

--- src/EEGModalNet/pipeline/train_gan_gpu.py
+++ src/EEGModalNet/pipeline/train_gan_gpu.py
@@ -87,7 +87,7 @@ def run(data,
                   epochs=max_epochs,
                   shuffle=True,
                   callbacks=[
-                      CustomModelCheckpoint(model_path, save_freq=20),
+                      CustomModelCheckpoint(model_path, save_freq=50),
                       keras.callbacks.ModelCheckpoint(f'{model_path}_best_gloss.model.keras', monitor='2 g_loss', save_best_only=True),
                       keras.callbacks.ModelCheckpoint(f'{model_path}_best_dloss.model.keras', monitor='1 d_loss', save_best_only=True),
                       keras.callbacks.CSVLogger(cvloger_path),
@@ -126,11 +126,11 @@ if __name__ == '__main__':
     keras.mixed_precision.set_global_policy('mixed_float16')
     print(f'Global policy is {keras.mixed_precision.global_policy().name}')
 
-    output_path = 'logs/04.02.2025_final-ks1'
+    output_path = 'logs/04.02.2025_skip-block'
 
     model = run(data,
                 n_subjects=n_subs,
-                max_epochs=2000,
+                max_epochs=3000,
                 latent_dim=128,
                 batch_size=128,
                 cvloger_path=f'{output_path}.csv',

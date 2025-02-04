20ea2652d859392ee8c36c62d535fcd31ad9e1eb
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-02-04 19:33:36 +0100

    correct data file path

--- src/EEGModalNet/pipeline/train_gan.py
+++ src/EEGModalNet/pipeline/train_gan.py
@@ -3,14 +3,9 @@ os.environ['KERAS_BACKEND'] = 'torch'
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 import torch
-import torch.multiprocessing as mp
 import keras
 from ...EEGModalNet import WGAN_GP
 from ...EEGModalNet import CustomModelCheckpoint
-import torch.distributed as dist
-import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.utils.data import DataLoader, DistributedSampler, TensorDataset
 from typing import List
 import numpy as np
 import xarray as xr
@@ -24,7 +19,7 @@ def find_channel_ids(dataarray, ch_names):
 def load_data(data_path: str,
               n_subjects: int = 202,
               channels: List[str] = ['all'],
-              bandpass_filter: List[int] = [1, 42],
+              highpass_filter: float = 0.5,
               time_dim: int = 1024,
               exclude_sub_ids=None) -> tuple:
 
@@ -37,18 +32,19 @@ def load_data(data_path: str,
     x = x.to_numpy()
     n_subjects = x.shape[0]
 
-    if bandpass_filter is not None:
-        sos = butter(4, bandpass_filter, btype='band', fs=128, output='sos')
+    if highpass_filter is not None:
+        sos = butter(4, highpass_filter, btype='high', fs=128, output='sos')
         x = sosfiltfilt(sos, x, axis=-1)
 
     x = torch.tensor(x.copy()).unfold(2, time_dim, time_dim).permute(0, 2, 3, 1).flatten(0, 1)  # TODO: copy was added because of an error, look into this
 
     sub = np.arange(0, n_subjects).repeat(x.shape[0] // n_subjects)[:, np.newaxis]
 
-    ch_ind = find_channel_ids(xarray, channels)
-    pos = xarray.ch_positions[ch_ind][None].repeat(x.shape[0], 0)
+    # ch_ind = find_channel_ids(xarray, channels)
+    # pos = xarray.ch_positions[ch_ind][None].repeat(x.shape[0], 0)
+    pos = torch.tensor(xarray.ch_positions[None].repeat(x.shape[0], 0))
 
-    return {'x': x, 'sub': torch.tensor(sub), 'pos': torch.tensor(pos)}, n_subjects
+    return {'x': x, 'sub': torch.tensor(sub), 'pos': pos}, n_subjects
 
 
 def run(data,
@@ -64,19 +60,20 @@ def run(data,
     model = WGAN_GP(time_dim=512, feature_dim=data['x'].shape[-1],
                     latent_dim=latent_dim, n_subjects=n_subjects,
                     use_sublayer_generator=True,
-                    use_sublayer_critic=False,
-                    use_channel_merger_c=True,
-                    use_channel_merger_g=True,
+                    use_sublayer_critic=True,
+                    use_channel_merger_c=False,
+                    use_channel_merger_g=False,
                     kerner_initializer='random_normal',
                     interpolation='bilinear')
 
-    model.compile(d_optimizer=keras.optimizers.Adam(0.00005, beta_1=0.5, beta_2=0.9),
-                  g_optimizer=keras.optimizers.Adam(0.0005, beta_1=0.5, beta_2=0.9),
-                  gradient_penalty_weight=5.0)
+    model.compile(d_optimizer=keras.optimizers.Adam(0.0000940, beta_1=0.5, beta_2=0.9),
+                  g_optimizer=keras.optimizers.Adam(0.0000940, beta_1=0.5, beta_2=0.9),
+                  gradient_penalty_weight=10.0)
 
-    callbacks = [CustomModelCheckpoint(model_path, save_freq=20),
+    callbacks = [CustomModelCheckpoint(model_path, save_freq=50),
                  keras.callbacks.CSVLogger(cvloger_path, append=True),
-                 keras.callbacks.ModelCheckpoint(model_path + 'best_gloss.model.keras', monitor='g_loss', save_best_only=True)]
+                 keras.callbacks.ModelCheckpoint(model_path + 'best_gloss.model.keras', monitor='g_loss', save_best_only=True),
+                 keras.callbacks.TerminateOnNaN()]
 
     _ = model.fit(data,
                   epochs=max_epochs,
@@ -87,20 +84,20 @@ def run(data,
 
 
 if __name__ == '__main__':
-    data, n_subs = load_data('data/LEMON_DATA/eeg_EC_BaseCorr_Norm_Clamp_with_pos.nc5',
-                             n_subjects=34, channels=['O1', 'O2', 'Fp1', 'Fp2', 'C1', 'C2', 'P1', 'P2'], bandpass_filter=[1, 42], time_dim=512,  # TODO: n_subjects=202
-                             # exclude_sub_ids=['sub-010257', 'sub-010044', 'sub-010266']
+    data, n_subs = load_data('data/LEMON_DATA/EC_8_channels_processed.nc5',
+                             n_subjects=202, channels=['O1', 'O2', 'F1', 'F2', 'C1', 'C2', 'P1', 'P2'],
+                             highpass_filter=0.5, time_dim=512,
                              )
 
     output_path = 'logs/multichannel_test_08-01-2025'
 
-    max_epochs = 100
-    latent_dim = 64
+    max_epochs = 2000
+    latent_dim = 128
     run(data,
         n_subs,
         max_epochs=max_epochs,
         latent_dim=latent_dim,
-        batch_size=64,
+        batch_size=128,
         cvloger_path=f'{output_path}.csv',
         model_path=output_path,
         reuse_model=False,

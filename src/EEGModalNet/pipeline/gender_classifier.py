40b4863e9fc9c0fbb3becca316d70df48cfb8e3d
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-02-20 11:49:46 +0100

    add gender classifier pipeline

--- src/EEGModalNet/pipeline/gender_classifier.py
+++ src/EEGModalNet/pipeline/gender_classifier.py
@@ -8,6 +8,7 @@ import xarray as xr
 from ...EEGModalNet import WGAN_GP_old
 from scipy.signal import butter, sosfiltfilt
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import GroupShuffleSplit
 
 
@@ -17,8 +18,10 @@ def load_data(data_path: str,
               time_dim: int = 1024,
               exclude_sub_ids=None) -> tuple:
 
+    channels = ['O1', 'O2', 'F1', 'F2', 'C1', 'C2', 'P1', 'P2']
+
     xarray = xr.open_dataarray(data_path, engine='h5netcdf')
-    x = xarray.sel(subject=xarray.subject[:n_subjects])
+    x = xarray.sel(subject=xarray.subject[:n_subjects], channel=channels)
 
     if exclude_sub_ids is not None:
         x = x.sel(subject=~x.subject.isin(exclude_sub_ids))
@@ -30,13 +33,10 @@ def load_data(data_path: str,
         sos = butter(4, bandpass_filter, btype='high', fs=128, output='sos')
         x = sosfiltfilt(sos, x, axis=-1)
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    x = torch.tensor(x.copy(), device=device).unfold(2, time_dim, time_dim).permute(0, 2, 3, 1).flatten(0, 1)  # TODO: copy was added because of an error, look into this
-    # reorder the channels from frontal to occipital
-    # new_order = [0, 1, 6, 7, 4, 5, 2, 3]
-    # x = x[:, :, new_order]
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    x = torch.tensor(x.copy()).unfold(2, time_dim, time_dim).permute(0, 2, 3, 1).flatten(0, 1)  # TODO: copy was added because of an error, look into this
 
-    sub = torch.tensor(np.arange(0, n_subjects).repeat(x.shape[0] // n_subjects)[:, np.newaxis], device=device)
+    sub = torch.tensor(np.arange(0, n_subjects).repeat(x.shape[0] // n_subjects)[:, np.newaxis])
     labels = xarray.gender - 1
     y = labels.repeat(x.shape[0]//202)
     sub_ids_classifier = sub.squeeze().numpy()
@@ -46,14 +46,16 @@ def load_data(data_path: str,
 
 if __name__ == '__main__':
 
-    X_input, y, groups = load_data('data/LEMON_DATA/EC_8_channels_processed_downsampled.nc5',
-                             n_subjects=202,
-                             bandpass_filter=0.5,
-                             time_dim=512,
-                             exclude_sub_ids=None)
-    
+    X_input, y, groups = load_data('data/LEMON_DATA/EC_all_channels_processed_downsampled.nc5',
+                                   n_subjects=202,
+                                   bandpass_filter=0.5,
+                                   time_dim=512,
+                                   exclude_sub_ids=None)
+
     group_shuffle = GroupShuffleSplit(n_splits=1, test_size=0.2, random_state=5)  # random state == 9
     train_idx, val_idx = next(group_shuffle.split(X_input, y, groups=groups))
+    print('Chance level',
+          np.unique(y[train_idx], return_counts=True)[1] / len(y[train_idx]), np.unique(y[val_idx], return_counts=True)[1] / len(y[val_idx]))
 
     from sklearn.utils import class_weight
     class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y), y=y)
@@ -66,11 +68,12 @@ if __name__ == '__main__':
                         use_channel_merger_g=False,
                         use_channel_merger_c=False,
                         interpolation='bilinear')
-    
+
     model.load_weights('logs/6/06.02.2025_epoch_2500.model.keras')
     critic = model.critic.model
 
     critic_output = critic.get_layer('dis_flatten').output  # the 4096-dim layer
+    critic_output = keras.layers.Dropout(0.2)(critic_output)
     new_output = keras.layers.Dense(1, activation='sigmoid', name='classification_head')(critic_output)
 
     new_model = keras.Model(inputs=critic.layers[0].input, outputs=new_output)
@@ -85,11 +88,10 @@ if __name__ == '__main__':
                       metrics=['accuracy', 'AUC'])
 
     history = new_model.fit(X_input[train_idx], y[train_idx],
-                            epochs=1,
+                            epochs=1000,
                             batch_size=128,
                             validation_data=(X_input[val_idx], y[val_idx]),
                             class_weight=class_weights
                             )
 
-    import pandas as pd
     pd.DataFrame(history.history).to_csv('logs/geneder_classification.csv')

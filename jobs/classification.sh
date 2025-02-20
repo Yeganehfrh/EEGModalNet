662b8e140814043a777902758a867148091d76a2
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-02-20 12:13:11 +0100

    add SLURM job script for training gender classifier

---
+++ jobs/classification.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#SBATCH --job-name=train_classifier
+#SBATCH --chdir=//work/projects/acnets/EEGModalNet/
+#SBATCH --partition=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=16GB
+#SBATCH --time=4:00:00
+#SBATCH --output=/work/projects/acnets/EEGModalNet/logs/classifier_%j.log
+#SBATCH --error=/work/projects/acnets/EEGModalNet/logs/classifier_%j.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=y.farahzadi@gmail.com
+
+alias micromamba=~/.local/bin/micromamba
+
+# SECTION Run pipeline
+micromamba run -n EEGModalNet python -m src.EEGModalNet.pipeline.gender_classifier

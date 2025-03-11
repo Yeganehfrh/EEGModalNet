332ea49ec96c4a4d4b868da2fd3685f15a8f7e3c
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-03-11 11:30:48 +0100

    remove commented-out code from Generator and Critic implementation

--- src/EEGModalNet/models/WGAN.py
+++ src/EEGModalNet/models/WGAN.py
@@ -46,18 +46,6 @@ class Critic(keras.Model):
             layers.Dense(1, name='dis_dense6', dtype='float32', kernel_initializer=kernel_initializer),
         ], name='critic')
 
-        # self.model = keras.Sequential([
-        #     keras.Input(shape=self.input_shape),
-        #     LearnablePositionalEmbedding(512, 8),
-        #     SelfAttention1D(2, 4),
-        #     StridedResidualBlock(feature_dim, kernel_size=3, strides=2, kernel_initializer=kernel_initializer, activation=keras.layers.LeakyReLU(0.1)),
-        #     SelfAttention1D(4, feature_dim),
-        #     layers.Conv1D(16 * feature_dim, ks, strides=2, padding='same', name='conv6', kernel_initializer=kernel_initializer),
-        #     layers.LeakyReLU(negative_slope=negative_slope),
-        #     layers.Flatten(name='dis_flatten'),
-        #     layers.Dense(1, name='dis_dense6', dtype='float32', kernel_initializer=kernel_initializer),
-        # ], name='critic')
-
         self.built = True
 
     def call(self, inputs):
@@ -120,25 +108,6 @@ class Generator(keras.Model):
                        negative_slope=0.2,
                        kernel_initializer=kernel_initializer,
                        batch_norm=True),
-            # ConvBlockResidual(filters=2 * [8 * feature_dim],
-            #                   kernel_sizes= 2 * [3],
-            #                   upsampling=[1, 1],
-            #                   stride=1,
-            #                   padding='same',
-            #                   interpolation=interpolation,
-            #                   negative_slope=0.2,
-            #                   kernel_initializer=kernel_initializer,
-            #                   batch_norm=True),
-            # SelfAttention1D(4, 8),
-            # ConvBlockResidual(filters=2 * [8 * feature_dim],
-            #                   kernel_sizes= 2 * [3],
-            #                   upsampling=[0, 0],
-            #                   stride=1,
-            #                   padding='same',
-            #                   interpolation=interpolation,
-            #                   negative_slope=0.2,
-            #                   kernel_initializer=kernel_initializer,
-            #                   batch_norm=True),
             SelfAttention1D(4, 16),
             layers.Conv1D(feature_dim, 3, padding='same', name='conv_lyr_1', kernel_initializer=kernel_initializer),
         ], name='generator')

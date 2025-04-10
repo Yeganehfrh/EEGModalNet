cbbe36a8f488d890f96e54577dd3e18cb002a9f3
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-04-10 15:58:50 +0200

    update memory allocation to 32 gb

--- jobs/train_gan_gpu.sh
+++ jobs/train_gan_gpu.sh
@@ -11,6 +11,7 @@
 #SBATCH --error=/work/projects/acnets/EEGModalNet/logs/train_gan_pipeline_%j.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=y.farahzadi@gmail.com
+#SBATCH --mem=32G
 
 alias micromamba=~/.local/bin/micromamba
 

8c6bd7aa26af2b6b4b7993ed579b65d0cbba8ac0
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-04-11 14:51:00 +0200

    Implement dynamic critic update frequency based on Wasserstein distance

--- src/EEGModalNet/pipeline/train_gan_gpu.py
+++ src/EEGModalNet/pipeline/train_gan_gpu.py
@@ -130,7 +130,7 @@ if __name__ == '__main__':
     keras.mixed_precision.set_global_policy('mixed_float16')
     print(f'Global policy is {keras.mixed_precision.global_policy().name}')
 
-    output_path = 'logs/11.04.2025'
+    output_path = 'logs/11.04.2025_dynamic_C_update'
 
     model = run(data,
                 n_subjects=n_subs,

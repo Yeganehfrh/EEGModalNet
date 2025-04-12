7b611ebcebd75089318de8f2d66dc0cf053bfdd2
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-04-12 09:55:10 +0200

    Update output path

--- src/EEGModalNet/pipeline/train_gan_gpu.py
+++ src/EEGModalNet/pipeline/train_gan_gpu.py
@@ -130,7 +130,7 @@ if __name__ == '__main__':
     keras.mixed_precision.set_global_policy('mixed_float16')
     print(f'Global policy is {keras.mixed_precision.global_policy().name}')
 
-    output_path = 'logs/11.04.2025_dynamic_C_update'
+    output_path = 'logs/12.04.2025'
 
     model = run(data,
                 n_subjects=n_subs,

772d95a13e64ee7837f8fa696bb189f39ca5b82e
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-04-13 12:05:01 +0200

    Comment out CUDA device checks and related print statements

--- src/EEGModalNet/pipeline/train_gan_gpu.py
+++ src/EEGModalNet/pipeline/train_gan_gpu.py
@@ -108,13 +108,13 @@ if __name__ == '__main__':
 
     if torch.cuda.is_available():
         print('GPU is available')
-        torch.cuda.current_device()
+        # torch.cuda.current_device()
     else:
         print('GPU is not available!!')
         exit()
 
     print(f'Running on {torch.cuda.device_count()} GPUs')
-    print(f'Using CUDA device: {torch.cuda.get_device_name(0)}')
+    # print(f'Using CUDA device: {torch.cuda.get_device_name(0)}')
 
     # Explicitly set the CUDA device
     torch.cuda.set_device(0)

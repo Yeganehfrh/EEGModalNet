64397db02961b684cb0478e769a269e616855296
Morteza Ansarinia <ansarinia@me.com>
2025-04-13 12:58:19 +0200

    revert back naming

--- jobs/train_gan_gpu.sh
+++ jobs/train_gan_gpu.sh
@@ -17,4 +17,4 @@ alias micromamba=~/.local/bin/micromamba
 git pull
 
 # SECTION Run pipeline
-micromamba run -n EEGModalNet python -m src.EEGModalNet.pipeline.train_gan_hpc
+micromamba run -n EEGModalNet python -m src.EEGModalNet.pipeline.train_gan_gpu

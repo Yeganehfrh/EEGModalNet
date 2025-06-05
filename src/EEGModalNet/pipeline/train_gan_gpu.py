1d1acd964e03a7a96f46f451816ea47fd70291ac
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-06-05 20:58:49 +0200

    use random subject labels in WGAN_GP_V0 for both generator and critic

--- src/EEGModalNet/pipeline/train_gan_gpu.py
+++ src/EEGModalNet/pipeline/train_gan_gpu.py
@@ -139,7 +139,7 @@ if __name__ == '__main__':
     N_SUBJECTS = 202
     LATENT_DIM = 128
     BATCH_SIZE = 128
-    OUTPUT_PATH = 'logs/20250605_7th'
+    OUTPUT_PATH = 'logs/20250605_7th_modified'
     CONDITION = None
 
     data = load_data('data/LEMON_DATA/EC_ch-8_sf-128.nc5',

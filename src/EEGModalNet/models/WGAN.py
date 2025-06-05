ec9fafc1baf57608329dffbbbf56c19b7796e80b
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-06-05 13:37:47 +0200

    add spectral regularization loss to critic's loss

--- src/EEGModalNet/models/WGAN.py
+++ src/EEGModalNet/models/WGAN.py
@@ -243,7 +243,7 @@ class WGAN_GP(keras.Model):
         fake_pred = self.critic({'x': fake_data, 'sub': random_sub, 'pos': pos})  # TODO: should we use the same sub and pos for fake data?
         gp = self.gradient_penalty(real_data, fake_data.detach(), sub, pos)
         self.zero_grad()
-        d_loss = (fake_pred.mean() - real_pred.mean()) + gp * self.gradient_penalty_weight
+        d_loss = (fake_pred.mean() - real_pred.mean()) + gp * self.gradient_penalty_weight + spectral_regularization_loss(real_data, fake_data)
         d_loss.backward()
 
         # clip gradients

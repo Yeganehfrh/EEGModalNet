7a00bf14619c8f6faf367bf52494db002520d81b
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-06-05 16:31:25 +0200

    update output path

--- src/EEGModalNet/pipeline/train_gan_gpu.py
+++ src/EEGModalNet/pipeline/train_gan_gpu.py
@@ -128,7 +128,7 @@ if __name__ == '__main__':
     N_SUBJECTS = 202
     LATENT_DIM = 128
     BATCH_SIZE = 128
-    OUTPUT_PATH = 'logs/20250605'
+    OUTPUT_PATH = 'logs/20250605_modi_upsample'
     CONDITION = 'eye_closed'
 
     data = load_data('data/LEMON_DATA/EO-EC_processed_ch-16_sf-128.nc5',

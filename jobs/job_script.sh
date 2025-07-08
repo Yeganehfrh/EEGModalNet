80afd1ec8089989a4d75a328f24a2a5227c16321
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-07-08 17:07:27 +0200

    reduce time allocation  to 4 hrs

--- jobs/job_script.sh
+++ jobs/job_script.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks=1
 #SBATCH --mem=16GB
-#SBATCH --time=12:00:00
+#SBATCH --time=4:00:00
 #SBATCH --output=/work/projects/acnets/EEGModalNet/logs/IG_pipeline_%j.log
 #SBATCH --error=/work/projects/acnets/EEGModalNet/logs/IG_pipeline_%j.log
 #SBATCH --mail-type=ALL

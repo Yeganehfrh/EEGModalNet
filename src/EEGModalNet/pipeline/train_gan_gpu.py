d9c32d553edb9cb0baa57b951579882bd4b84ff5
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-07-08 15:02:46 +0200

    Critic: update SelfAttention1D configuration + remove conv layers

--- src/EEGModalNet/pipeline/train_gan_gpu.py
+++ src/EEGModalNet/pipeline/train_gan_gpu.py
@@ -139,7 +139,7 @@ if __name__ == '__main__':
     N_SUBJECTS = 202
     LATENT_DIM = 128
     BATCH_SIZE = 128
-    OUTPUT_PATH = 'logs/20250708'
+    OUTPUT_PATH = 'logs/20250708_crt_transformer'
     CONDITION = None
 
     data = load_data('data/LEMON_DATA/EC_ch-8_sf-128.nc5',

d1e96344271911a06e405c9a31666d9ef778f8fc
Yeganeh Farahzadi <y.farahzadi@gmail.com>
2025-08-06 13:03:44 +0200

    update print statements + output path

--- jobs/classification.sh
+++ jobs/classification.sh
@@ -13,4 +13,4 @@
 #SBATCH --mail-user=y.farahzadi@gmail.coml
 
 # SECTION Run pipeline
-pixi run python -m src.EEGModalNet.pipeline.gender_classifier --classifier Convolution --model-path 'logs/gender_cls_20250806_OTKA_yaregan_conv'
+pixi run python -m src.EEGModalNet.pipeline.gender_classifier --classifier Convolution --use-raw --model-path 'logs/gender_cls_20250806_OTKA_raw_conv'

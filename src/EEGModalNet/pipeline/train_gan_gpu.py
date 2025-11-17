ae1640eadcbab2b2318975621dbc927f879fc4ef
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2025-11-17 21:52:59 +0100

    fix model checkpoint mode to 'min'

--- src/EEGModalNet/pipeline/train_gan_gpu.py
+++ src/EEGModalNet/pipeline/train_gan_gpu.py
@@ -120,8 +120,8 @@ def run(data,
                   shuffle=True,
                   callbacks=[
                       CustomModelCheckpoint(model_path, save_freq=20),
-                      keras.callbacks.ModelCheckpoint(f'{model_path}_best_gloss.model.keras', monitor='2 g_loss', save_best_only=True),
-                      keras.callbacks.ModelCheckpoint(f'{model_path}_best_dloss.model.keras', monitor='1 d_loss', save_best_only=True),
+                      keras.callbacks.ModelCheckpoint(f'{model_path}_best_gloss.model.keras', monitor='2 g_loss', save_best_only=True, mode='min'),
+                      keras.callbacks.ModelCheckpoint(f'{model_path}_best_dloss.model.keras', monitor='1 d_loss', save_best_only=True, mode='min'),
                       keras.callbacks.CSVLogger(cvloger_path),
                       keras.callbacks.TerminateOnNaN()
                       # step_loss_history

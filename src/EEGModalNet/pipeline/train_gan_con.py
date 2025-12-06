b86d8294a1d01704adc7e3ee379108360164ba60
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2025-12-06 11:22:10 +0100

    update LR and GP

--- src/EEGModalNet/pipeline/train_gan_con.py
+++ src/EEGModalNet/pipeline/train_gan_con.py
@@ -93,12 +93,12 @@ def run(train_loader,
         print(reuse_model_path)
         model.load_weights(reuse_model_path)
 
-    lr_schedule_g = ExponentialDecay(0.0002, decay_steps=100000, decay_rate=0.90, staircase=True)
-    lr_schedule_d = ExponentialDecay(0.0004, decay_steps=100000, decay_rate=0.90, staircase=True)
+    lr_schedule_g = ExponentialDecay(0.000188, decay_steps=100000, decay_rate=0.90, staircase=True)
+    lr_schedule_d = ExponentialDecay(0.000188*2, decay_steps=100000, decay_rate=0.90, staircase=True)
 
     model.compile(d_optimizer=keras.optimizers.Adam(lr_schedule_d, beta_1=0.5, beta_2=0.9),
                   g_optimizer=keras.optimizers.Adam(lr_schedule_g, beta_1=0.5, beta_2=0.9),
-                  gradient_penalty_weight=1.0)
+                  gradient_penalty_weight=5.0)
 
     torch.cuda.synchronize()  # wait for model to be loaded
 

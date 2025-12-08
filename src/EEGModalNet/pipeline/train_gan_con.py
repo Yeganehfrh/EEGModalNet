e710ca70049d6bfd65b2ee6c337905fd7e87e023
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2025-12-08 21:12:33 +0100

    remove noise injection + reduce C updates

--- src/EEGModalNet/pipeline/train_gan_con.py
+++ src/EEGModalNet/pipeline/train_gan_con.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
     N_SUBJECTS = 202
     LATENT_DIM = 128
     BATCH_SIZE = 128
-    OUTPUT_PATH = 'logs/20251208_v2'
+    OUTPUT_PATH = 'logs/20251208_v3'
     CONDITION = None
 
     data = load_data('data/LEMON_DATA/EC_ch-8_sf-128.nc5',

18fc8cdf93e494d18e4765c05d528fa79e7a4d24
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2025-12-29 13:35:50 +0100

    increase n_samples in dataloder & GP weight + decrease lr_d

--- src/EEGModalNet/pipeline/train_gan_con.py
+++ src/EEGModalNet/pipeline/train_gan_con.py
@@ -131,11 +131,11 @@ def run(train_loader,
         model.load_weights(reuse_model_path)
 
     lr_schedule_g = ExponentialDecay(0.0002, decay_steps=100000, decay_rate=0.90, staircase=True)
-    lr_schedule_d = ExponentialDecay(0.0006, decay_steps=100000, decay_rate=0.90, staircase=True)
+    lr_schedule_d = ExponentialDecay(0.0004, decay_steps=100000, decay_rate=0.90, staircase=True)
 
     model.compile(d_optimizer=keras.optimizers.Adam(lr_schedule_d, beta_1=0.0, beta_2=0.9),
                   g_optimizer=keras.optimizers.Adam(lr_schedule_g, beta_1=0.0, beta_2=0.9),
-                  gradient_penalty_weight=1)
+                  gradient_penalty_weight=10)
 
     torch.cuda.synchronize()  # wait for model to be loaded
 
@@ -177,7 +177,7 @@ if __name__ == '__main__':
     N_SUBJECTS = 202
     LATENT_DIM = 128
     BATCH_SIZE = 128
-    OUTPUT_PATH = 'logs/20251228'
+    OUTPUT_PATH = 'logs/20251229'
     CONDITION = 'both' #FIX currently it only work with two conditions
 
     data = load_data('data/LEMON_DATA/EC_ch-8_sf-128.nc5',
@@ -198,7 +198,7 @@ if __name__ == '__main__':
     RandomCropEEGDataset(
         data['x'], data['sub'], data['pos'],
         seg_len=512,
-        n_samples=100_000   # 100k random crops per epoch
+        n_samples=500_000   # 100k random crops per epoch
     ),
     batch_size=128,
     shuffle=False,
@@ -206,6 +206,10 @@ if __name__ == '__main__':
     drop_last=True,
     generator=g
     )
+
+    print("len(dataset):", len(train_loader.dataset))
+    print("len(loader):", len(train_loader))
+    print("type passed to fit:", type(train_loader))
 
     if torch.cuda.is_available():
         print('GPU is available')

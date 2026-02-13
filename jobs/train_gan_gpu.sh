8b061fce68a38b8aff13f495e4606a1efdcc56df
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2026-02-13 12:08:24 +0100

    update paths

--- jobs/train_gan_gpu.sh
+++ jobs/train_gan_gpu.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 #SBATCH --job-name=train_gan_gpu
-#SBATCH --chdir=//work/projects/acnets/EEGModalNet/
+#SBATCH --chdir=EEGModalNet/
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --time=15:00:00
+#SBATCH --time=10:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --output=/work/projects/acnets/EEGModalNet/logs/train_gan_pipeline_%j.log
-#SBATCH --error=/work/projects/acnets/EEGModalNet/logs/train_gan_pipeline_%j.log
+#SBATCH --output=EEGModalNet/logs/train_gan_pipeline_%j.log
+#SBATCH --error=EEGModalNet/logs/train_gan_pipeline_%j.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=y.farahzadi@gmail.com
 

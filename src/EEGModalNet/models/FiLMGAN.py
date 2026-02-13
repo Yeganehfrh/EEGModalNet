1c9fd3cfc98f29bc9fc995317609b9326b62301e
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2026-02-13 14:40:58 +0100

    fix: handle data input format in train_step

--- src/EEGModalNet/models/FiLMGAN.py
+++ src/EEGModalNet/models/FiLMGAN.py
@@ -320,6 +320,8 @@ class FiLMGAN(keras.Model):
         return default if x is None else float(x.detach().cpu())
 
     def train_step(self, data):
+        if isinstance(data, (tuple, list)):
+            data = data[0]
         real_data, sub, pos = data['x'], data['sub'], data['pos']
 
         device = next(self.parameters()).device

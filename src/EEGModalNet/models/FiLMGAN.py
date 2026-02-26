fd087230472a3f35c0bff5a595846f681f4fc3e9
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2026-02-26 09:34:55 +0100

    widen conv layers in C + remove learnable postions

--- src/EEGModalNet/models/FiLMGAN.py
+++ src/EEGModalNet/models/FiLMGAN.py
@@ -19,7 +19,7 @@ class Critic(keras.Model):
         kernel_initializer = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
         self.d_sub = 32
         self.output_features = False
-        disable_attention = True
+        # disable_attention = True
 
         self.sub_emb = torch.nn.Embedding(n_subjects, self.d_sub)
         # self.state_emb = torch.nn.Embedding(2, 16)  # (number of states, emdding dimentions)
@@ -38,10 +38,10 @@ class Critic(keras.Model):
     
         self.conv1 = layers.Conv1D(feature_dim, ks, padding='same', name='conv1', kernel_initializer=kernel_initializer)
         self.act1  = layers.LeakyReLU(negative_slope=negative_slope)
-        self.conv2 = layers.Conv1D(2 * feature_dim, ks, strides=2, padding='same', name='conv2', kernel_initializer=kernel_initializer)
+        self.conv2 = layers.Conv1D(4 * feature_dim, ks, strides=2, padding='same', name='conv2', kernel_initializer=kernel_initializer)
         # self.pool2 = layers.AveragePooling1D(pool_size=2)
         self.act2  = layers.LeakyReLU(negative_slope=negative_slope)
-        self.conv3 = layers.Conv1D(8 * feature_dim, ks, strides=2, padding='same', name='conv3', kernel_initializer=kernel_initializer)
+        self.conv3 = layers.Conv1D(16 * feature_dim, ks, strides=2, padding='same', name='conv3', kernel_initializer=kernel_initializer)
         # self.pool3 = layers.AveragePooling1D(pool_size=2)
         self.act3  = layers.LeakyReLU(negative_slope=negative_slope)
         # self.att2  = SelfAttention1D(8, feature_dim, disable_attention=disable_attention)
@@ -49,7 +49,7 @@ class Critic(keras.Model):
         # self.pool4 = layers.AveragePooling1D(pool_size=2)
         # self.act4  = layers.LeakyReLU(negative_slope=negative_slope)
         self.flatten = layers.Flatten(name='dis_flatten')
-        self.final_dense = layers.Dense(1, name='dis_dense6', dtype='float32', kernel_initializer=kernel_initializer)
+        self.final_dense = layers.Dense(1, name='final_dense', dtype='float32', kernel_initializer=kernel_initializer)
 
         self.mbsdv = MinibatchStdDev()
 
@@ -97,11 +97,11 @@ class Critic(keras.Model):
         h1 = self.act1(self.conv1(x_cat))
         pooled_h1  = layers.MaxPool1D(pool_size=max(1, h1.shape[1]//4))(h1)
         pooled_h1 = layers.Flatten()(pooled_h1)
-        h = self.act2(self.pool2(self.conv2(h1)))
+        h = self.act2(self.conv2(h1))
         pooled_h  = layers.MaxPool1D(pool_size=max(1, h.shape[1]//4))(h)
         pooled_h = layers.Flatten()(pooled_h)
         feats = ops.concatenate([pooled_h1, pooled_h], axis=-1)
-        h = self.act3(self.pool3(self.conv3(h)))
+        h = self.act3(self.conv3(h))
         pooled_h  = layers.MaxPool1D(pool_size=max(1, h.shape[1]//4))(h)
         pooled_h = layers.Flatten()(pooled_h)
         feats = ops.concatenate([feats, pooled_h], axis=-1)
@@ -147,7 +147,7 @@ class Generator(keras.Model):
         self.input_shape = (time_dim, feature_dim)
         kernel_initializer = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
         self.d_sub = 32
-        disable_attention = True
+        # disable_attention = True
 
 
         self.sub_emb = torch.nn.Embedding(n_subjects, self.d_sub)
@@ -157,11 +157,12 @@ class Generator(keras.Model):
 
         self.post_att = keras.Sequential([
             keras.Input(shape=((latent_dim,))),
-            layers.Dense(4096 * 1, kernel_initializer=kernel_initializer, name='gen_layer5'),
-            layers.LeakyReLU(negative_slope=self.negative_slope, name='gen_layer6'),
-            layers.Reshape((128, 32), name='gen_layer9'),
-            LearnablePositionalEmbedding(128, 32),
-            SelfAttention1D(4, 8, disable_attention=disable_attention)])
+            layers.Dense(4096 * 1, kernel_initializer=kernel_initializer, name='gen_layer1'),
+            layers.LeakyReLU(negative_slope=self.negative_slope, name='gen_layer2'),
+            layers.Reshape((128, 32), name='gen_layer3'),
+            # LearnablePositionalEmbedding(128, 32),
+            # SelfAttention1D(4, 8, disable_attention=disable_attention)
+            ])
         
         self.film_block = FiLMBlock(32, 32)
 
@@ -175,9 +176,8 @@ class Generator(keras.Model):
                        padding='same',
                        interpolation=interpolation,
                        negative_slope=0.2,
-                       kernel_initializer=kernel_initializer,
-                       batch_norm=True),
-                       SelfAttention1D(4, 16, disable_attention=disable_attention),
+                       kernel_initializer=kernel_initializer, batch_norm=True),
+                    #    SelfAttention1D(4, 16, disable_attention=disable_attention),
                        layers.Conv1D(feature_dim, 3, padding='same', name='intermediate_conv', kernel_initializer=kernel_initializer),
                     #    layers.LeakyReLU(negative_slope=0.2),
         ], name='conv_block')

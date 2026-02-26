ca85941c7a89347445f337cae80cdf95378a8dd1
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2026-02-26 12:31:19 +0100

    train on both conditions with FiLM state conditioning enabled

--- src/EEGModalNet/models/FiLMGAN.py
+++ src/EEGModalNet/models/FiLMGAN.py
@@ -19,35 +19,23 @@ class Critic(keras.Model):
         kernel_initializer = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
         self.d_sub = 32
         self.output_features = False
-        # disable_attention = True
 
         self.sub_emb = torch.nn.Embedding(n_subjects, self.d_sub)
-        # self.state_emb = torch.nn.Embedding(2, 16)  # (number of states, emdding dimentions)
+        self.state_emb = torch.nn.Embedding(2, 16)  # (number of states, emdding dimentions)
         if use_sublayer:
-            self.sub_layer = SubjectLayers_FiLM(feature_dim, feature_dim, self.d_sub, init_id=True)
+            self.sub_layer = SubjectStateLayers_FiLM(feature_dim, self.d_sub, init_id=True)
 
         ks = 5
-
-        # self.post_att = keras.Sequential([
-        #     keras.Input(shape=self.input_shape),
-        #     LearnablePositionalEmbedding(512, 8),
-        #     SelfAttention1D(2, 4, disable_attention=disable_attention)])
         
-        self.film_block = FiLMBlock(8, 32)
+        self.film_block = DualFiLMBlock(8, 32)
         self.highpass = HighPass1D()
     
         self.conv1 = layers.Conv1D(feature_dim, ks, padding='same', name='conv1', kernel_initializer=kernel_initializer)
         self.act1  = layers.LeakyReLU(negative_slope=negative_slope)
         self.conv2 = layers.Conv1D(4 * feature_dim, ks, strides=2, padding='same', name='conv2', kernel_initializer=kernel_initializer)
-        # self.pool2 = layers.AveragePooling1D(pool_size=2)
         self.act2  = layers.LeakyReLU(negative_slope=negative_slope)
         self.conv3 = layers.Conv1D(16 * feature_dim, ks, strides=2, padding='same', name='conv3', kernel_initializer=kernel_initializer)
-        # self.pool3 = layers.AveragePooling1D(pool_size=2)
         self.act3  = layers.LeakyReLU(negative_slope=negative_slope)
-        # self.att2  = SelfAttention1D(8, feature_dim, disable_attention=disable_attention)
-        # self.conv4 = layers.Conv1D(16 * feature_dim, ks, padding='same', name='conv6', kernel_initializer=kernel_initializer)
-        # self.pool4 = layers.AveragePooling1D(pool_size=2)
-        # self.act4  = layers.LeakyReLU(negative_slope=negative_slope)
         self.flatten = layers.Flatten(name='dis_flatten')
         self.final_dense = layers.Dense(1, name='final_dense', dtype='float32', kernel_initializer=kernel_initializer)
 
@@ -58,11 +46,10 @@ class Critic(keras.Model):
     def call(self, inputs):
         x, sub_labels, state_id = inputs['x'], inputs['sub'], inputs['pos']
         subj_emb = self.sub_emb(sub_labels.view(-1))
-        # state_emb = self.state_emb(state_id.view(-1))
+        state_emb = self.state_emb(state_id.view(-1))
         if hasattr(self, 'sub_layer'):
-            x = self.sub_layer(x, subj_emb)
-        # x = self.post_att(x)
-        x = self.film_block(x, subj_emb)
+            x = self.sub_layer(x, subj_emb, state_emb)
+        x = self.film_block(x, subj_emb, state_emb)
 
         x_hp = self.highpass(x)        # (B, 512, 8), HF-emphasised
         x_cat = ops.concatenate([x, x_hp], axis=-1)  # (B, 512, 16)
@@ -70,8 +57,6 @@ class Critic(keras.Model):
         h1 = self.act1(self.conv1(x_cat))    # (B, 512, C1) HF-rich
         h  = self.act2(self.conv2(h1))
         h  = self.act3(self.conv3(h))
-        # h  = self.att2(h)
-        # h  = self.act4(self.pool4(self.conv4(h)))
 
         h = self.mbsdv(h)
         
@@ -85,11 +70,11 @@ class Critic(keras.Model):
         out = self.final_dense(h_final)
         return out.float()
     
-    def extract_features(self, x, sub_labels):
+    def extract_features(self, x, sub_labels, state_ids):
         subj_emb = self.sub_emb(ops.reshape(sub_labels, (-1,)))
-        # state_emb = self.state_emb(ops.reshape(state_ids, (-1,)))
-        x = self.sub_layer(x, subj_emb)
-        x = self.film_block(x, subj_emb)
+        state_emb = self.state_emb(ops.reshape(state_ids, (-1,)))
+        x = self.sub_layer(x, subj_emb, state_emb)
+        x = self.film_block(x, subj_emb, state_emb)
 
         x_hp = self.highpass(x)
         x_cat = ops.concatenate([x, x_hp], axis=-1)
@@ -106,19 +91,7 @@ class Critic(keras.Model):
         pooled_h = layers.Flatten()(pooled_h)
         feats = ops.concatenate([feats, pooled_h], axis=-1)
 
-        # h = self.att2(h)
-        # pooled_h  = layers.MaxPool1D(pool_size=max(1, h.shape[1]//4))(h)
-        # pooled_h = layers.Flatten()(pooled_h)
-        # feats = ops.concatenate([feats, pooled_h], axis=-1)
-        # h = self.act4(self.pool4(self.conv4(h)))
-        # pooled_h  = layers.MaxPool1D(pool_size=max(1, h.shape[1]//4))(h)
-        # # print(h.shape, pooled_h.shape)
-        # pooled_h = layers.Flatten()(pooled_h)
-        # pooled_h = ops.mean(h, axis=1)
-        # feats = ops.concatenate([feats, pooled_h], axis=-1)
-
         return feats  # shape (B, D_feat)
-
 
     def get_config(self):
         config = super().get_config()
@@ -147,24 +120,20 @@ class Generator(keras.Model):
         self.input_shape = (time_dim, feature_dim)
         kernel_initializer = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
         self.d_sub = 32
-        # disable_attention = True
-
 
         self.sub_emb = torch.nn.Embedding(n_subjects, self.d_sub)
-        # self.state_emb = torch.nn.Embedding(2, 16)  # (number of states, emdding dimentions)
+        self.state_emb = torch.nn.Embedding(2, 16)  # (number of states, emdding dimentions)
         if use_sublayer:
-            self.sub_layer = SubjectLayers_FiLM(feature_dim, feature_dim, self.d_sub, init_id=True)
+            self.sub_layer = SubjectStateLayers_FiLM(feature_dim, self.d_sub, init_id=True)
 
         self.post_att = keras.Sequential([
             keras.Input(shape=((latent_dim,))),
             layers.Dense(4096 * 1, kernel_initializer=kernel_initializer, name='gen_layer1'),
             layers.LeakyReLU(negative_slope=self.negative_slope, name='gen_layer2'),
             layers.Reshape((128, 32), name='gen_layer3'),
-            # LearnablePositionalEmbedding(128, 32),
-            # SelfAttention1D(4, 8, disable_attention=disable_attention)
             ])
         
-        self.film_block = FiLMBlock(32, 32)
+        self.film_block = DualFiLMBlock(32, 32)
 
         self.cov_block = keras.Sequential([
             keras.Input(shape=(128, 32)),
@@ -177,9 +146,7 @@ class Generator(keras.Model):
                        interpolation=interpolation,
                        negative_slope=0.2,
                        kernel_initializer=kernel_initializer, batch_norm=True),
-                    #    SelfAttention1D(4, 16, disable_attention=disable_attention),
                        layers.Conv1D(feature_dim, 3, padding='same', name='intermediate_conv', kernel_initializer=kernel_initializer),
-                    #    layers.LeakyReLU(negative_slope=0.2),
         ], name='conv_block')
 
         self.dil_block = keras.Sequential([
@@ -202,12 +169,12 @@ class Generator(keras.Model):
         noise, sub_labels, state_id = inputs
         x = self.post_att(noise)
         subj_emb = self.sub_emb(sub_labels.view(-1))
-        # state_emb = self.state_emb(state_id.view(-1))
-        x = self.film_block(x, subj_emb)
+        state_emb = self.state_emb(state_id.view(-1))
+        x = self.film_block(x, subj_emb, state_emb)
         x = self.cov_block(x)
         x = self.dil_block(x)
         if hasattr(self, 'sub_layer'):
-            x = self.sub_layer(x, subj_emb)
+            x = self.sub_layer(x, subj_emb, state_emb)
         if keras.mixed_precision.global_policy().name == 'mixed_float16':
             x = x.float()  # make sure the output is in float32 in mixed precision mode
         return x
@@ -316,9 +283,10 @@ class FiLMGAN(keras.Model):
         gradients = torch.autograd.grad(
             outputs=prob_interpolated,
             inputs=interpolated,
-            grad_outputs=torch.ones(prob_interpolated.size(), device=real_data.device),
+            grad_outputs=torch.ones_like(prob_interpolated),
             create_graph=True,
             retain_graph=True,
+            only_inputs=True
         )[0]
         gradients = gradients.reshape(batch_size, -1)
         gradient_norm = gradients.norm(2, dim=1)
@@ -330,7 +298,9 @@ class FiLMGAN(keras.Model):
         if not torch.isfinite(t).all():
             print("NaNs at:", name, "max", t.abs().max().item())
             raise RuntimeError
-        
+
+    def safe_scalar(x, default=float("nan")):
+        return default if x is None else float(x.detach().cpu())
 
     def train_step(self, data):
         if isinstance(data, (tuple, list)):

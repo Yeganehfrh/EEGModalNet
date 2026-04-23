3c0e78da1c40242ae8c506875ee96ed0a55654ab
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2026-04-23 17:21:11 +0200

    use bash and modify pipeline commands for gender and age tasks

--- jobs/classification.sh
+++ jobs/classification.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #SBATCH --job-name=train_classifier
 #SBATCH --chdir=//work/projects/acnets/EEGModalNet/
@@ -10,7 +10,14 @@
 #SBATCH --output=/work/projects/acnets/EEGModalNet/logs/classifier_%j.log
 #SBATCH --error=/work/projects/acnets/EEGModalNet/logs/classifier_%j.log
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=y.farahzadi@gmail.coml
+#SBATCH --mail-user=y.farahzadi@gmail.com
+
+set -euo pipefail
 
 # SECTION Run pipeline
-pixi run python -m src.EEGModalNet.pipeline.gender_classifier --classifier Convolution --use-cbramod --model-path 'logs/gender_cls_20250806_OTKA_Cbra-4s_conv'
+pixi run python -m src.EEGModalNet.pipeline.classifier --features yaregan --task gender --n-epochs 1000 --batch-size 128
+pixi run python -m src.EEGModalNet.pipeline.classifier --features cbra --task gender --n-epochs 1000 --batch-size 128
+pixi run python -m src.EEGModalNet.pipeline.classifier --features raw --task gender --n-epochs 1000 --batch-size 128
+pixi run python -m src.EEGModalNet.pipeline.classifier --features yaregan --task age --n-epochs 1000 --batch-size 128
+pixi run python -m src.EEGModalNet.pipeline.classifier --features cbra --task age --n-epochs 1000 --batch-size 128
+pixi run python -m src.EEGModalNet.pipeline.classifier --features raw --task age --n-epochs 1000 --batch-size 128

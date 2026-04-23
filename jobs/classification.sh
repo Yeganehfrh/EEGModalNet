3b467685ab80b3c48c6b4bfd50efc85db5601cce
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2026-04-23 17:26:29 +0200

    correct project path

--- jobs/classification.sh
+++ jobs/classification.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=train_classifier
-#SBATCH --chdir=//work/projects/acnets/EEGModalNet/
+#SBATCH --chdir=/home/users/mansarinia/EEGModalNet/
 #SBATCH --partition=batch
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=4:00:00
 #SBATCH --mem=16GB
-#SBATCH --output=/work/projects/acnets/EEGModalNet/logs/classifier_%j.log
-#SBATCH --error=/work/projects/acnets/EEGModalNet/logs/classifier_%j.log
+#SBATCH --output=/home/users/mansarinia/EEGModalNet/logs/classifier_%j.log
+#SBATCH --error=/home/users/mansarinia/EEGModalNet/logs/classifier_%j.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=y.farahzadi@gmail.com
 
